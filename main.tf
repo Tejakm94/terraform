28670c3ac60de52a54f6af5676508c1f81600b68
Teja MANJUNATHA <teja.manjunatha@alten.com>
2026-01-08 11:03:46 +0530

    Initial terraform files

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_instance" "example" {
+  count = 1
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
